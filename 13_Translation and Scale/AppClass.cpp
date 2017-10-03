e580f41a239e18096459ded4b850eef57f5ea8f1
MAIN\nrw6218 <nrw6218@rit.edu>
2017-10-03 10:43:44 -0400

    Finished E05

--- 13_Translation and Scale/AppClass.cpp
+++ 13_Translation and Scale/AppClass.cpp
@@ -7,10 +7,12 @@ void Application::InitVariables(void)
 	////Alberto needed this at this position for software recording.
 	//m_pWindow->setPosition(sf::Vector2i(710, 0));
 	
+	m_pCameraMngr->SetPositionTargetAndUp(vector3(0.0f,0.0f,10.0f),vector3(0.0f,0.0f,0.0f),vector3(0.0f,1.0f,0.0f));
+
 	//init the mesh
 	m_pMesh = new MyMesh();
 	//m_pMesh->GenerateCube(1.0f, C_WHITE);
-	m_pMesh->GenerateSphere(1.0f, 5, C_WHITE);
+	m_pMesh->GenerateCone(1.0f, 1.5f, 10, C_PURPLE);
 }
 void Application::Update(void)
 {
@@ -30,16 +32,44 @@ void Application::Display(void)
 
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
-	
-	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
-	static float value = 0.0f;
-	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
-	value += 0.01f;
+	matrix4 m4Model = IDENTITY_M4;
 
-	//matrix4 m4Model = m4Translate * m4Scale;
-	matrix4 m4Model = m4Scale * m4Translate;
+	static DWORD startTime = GetTickCount();
+	DWORD currentTime = GetTickCount();
+	float fCurrentTime = (currentTime - startTime) / 1000.0f;
+
+	float fPercent = MapValue(fCurrentTime, 0.0f, 5.0f, 0.0f, 1.0f);;
+
+	vector3 v3Start = vector3(0.0f, 0.0f, 0.0f);
+	vector3 v3Stop1 = vector3(5.0f, 0.0f, 0.0f);
+	vector3 v3Stop2 = vector3(0.0f, 3.0f, 0.0f);
+	vector3 v3Current = glm::lerp(v3Start, v3Stop1, fPercent);
+		
+		//(v3Start + v3End) * fPercent;
+
+	m4Model = glm::translate(IDENTITY_M4, v3Current);
+
+	//if (fPercent < 1) {
+	//	fPercent += 0.01f;
+	//}
+	//else {
+	//	fPercent = 0.0f;
+	//}
+	
 
 	m_pMesh->Render(m4Projection, m4View, m4Model);
+
+	#pragma region Debugging Information
+		//Print info on the screen
+		uint nEmptyLines = 21;
+		for (uint i = 0; i < nEmptyLines; ++i)
+			m_pMeshMngr->PrintLine("");//Add a line on top
+									   //m_pMeshMngr->Print("						");
+
+		//m_pMeshMngr->Print("						");
+		m_pMeshMngr->Print("Current Time: ");//Add a line on top
+		m_pMeshMngr->PrintLine(std::to_string(fCurrentTime), C_YELLOW);
+	#pragma endregion
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
